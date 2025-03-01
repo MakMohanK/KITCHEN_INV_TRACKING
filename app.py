8b5f4b630b613efc3e5602fa9f44d6a2307f87dc
mohan kshirsagar <k94mak@gmail.com>
2025-03-01 15:18:36 +0530

    adding the edit record option

--- app.py
+++ app.py
@@ -38,5 +38,40 @@ def show_inventory():
     conn.close()
     return render_template('inventory.html', items=items)
 
+@app.route('/edit/<int:item_id>')
+def edit_item(item_id):
+    conn = get_db_connection()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("SELECT * FROM inventory WHERE Id = %s", (item_id,))
+    item = cursor.fetchone()
+    conn.close()
+
+    if not item:
+        return "Item not found", 404
+
+    return render_template('edit.html', item=item)
+
+@app.route('/update/<int:item_id>', methods=['POST'])
+def update_item(item_id):
+    item_name = request.form['item_name']
+    description = request.form['description']
+    weight = request.form['weight']
+    price = request.form['price']
+    mfg_date = request.form['mfg_date']
+    exp_date = request.form['exp_date']
+    nutritiants = request.form['nutritiants']
+    
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        UPDATE inventory
+        SET Item_Name=%s, Description=%s, Weight=%s, Price=%s, Mfg_Date=%s, Exp_Date=%s, Nutritiants=%s
+        WHERE Id=%s
+    ''', (item_name, description, weight, price, mfg_date, exp_date, nutritiants, item_id))
+    conn.commit()
+    conn.close()
+    
+    return redirect('/inventory')
+
 if __name__ == '__main__':
     app.run(debug=True)
